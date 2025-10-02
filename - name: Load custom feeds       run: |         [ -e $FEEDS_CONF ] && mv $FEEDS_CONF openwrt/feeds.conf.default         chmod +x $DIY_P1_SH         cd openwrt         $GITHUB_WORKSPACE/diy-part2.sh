02530a5e66f009e39dacf39b325e866764ef32b1
Type010 <xioayy_h@163.com>
2025-10-02 16:42:07 +0800

    Delete - name: Load custom feeds       run: |         [ -e $FEEDS_CONF ] && mv $FEEDS_CONF openwrt directory

--- - name: Load custom feeds       run: |         [ -e $FEEDS_CONF ] && mv $FEEDS_CONF openwrt/feeds.conf.default         chmod +x $DIY_P1_SH         cd openwrt         $GITHUB_WORKSPACE/diy-part2.sh
+++
@@ -1,4 +0,0 @@
-sed -i '/label = "ubi"/{n;s/reg = <0x5c0000 0x[0-9a-f]\+>/reg = <0x5c0000 0x7000000>/}' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dts
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/cudy/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
